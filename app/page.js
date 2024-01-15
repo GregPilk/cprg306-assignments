e79a06639ec05070336225348f8b5015341ddc88
Greg Pilkington <greg.j.pilkington@gmail.com>
2024-01-15 13:29:04 -0700

    License added
    Added License

--- app/page.js
+++ app/page.js
@@ -4,10 +4,10 @@ export default function Page(){
   return(
     <div>
       <main > 
-        <h1 class='text -2xl text-red-800 font-serif'>Web Dev 2 Assignments</h1>Hello World!
+        <h1 className='text-4xl text-green-500'>Web Dev 2 Assignments</h1>
         <ul>
           <li>
-            <Link href="week-2">Week 2</Link>
+            <Link href="week-2">Week 2 Assignments</Link>
           </li>
         </ul>  
       </main>
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,15 +1,9 @@
-import { Heading } from "./heading";
 import { StudentInfo } from "./student-info";
 
 export default function Page(){
-    let a = 5;
-    let b = 10;
-    let c = a + b;
     return(
         <main>
-            <Heading />
-            <h1>Student Info</h1>
-            <p>This is the sum of a and b is {c}</p>
+            <h1>Shopping List</h1>
             <StudentInfo />
         </main>
     );
